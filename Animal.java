fecb883eeb11c5bdc3c32fe872b5a30303490141
rustam <rusitama@gmail.com>
2023-07-10 12:03:13 +0500

    Add file Animal.java

---
+++ Animal.java
@@ -0,0 +1,16 @@
+// Родительский класс "Животное"
+public class Animal {
+    private String name;
+    
+    public Animal(String name) {
+        this.name = name;
+    }
+    
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+    
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+}
